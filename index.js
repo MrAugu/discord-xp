2f8d29d2926992c78475f47b519bd40798a6706b
MrAugu <mraugu@yahoo.com>
2019-09-27 15:21:06 +0300

    createUser & deleteUser methods

--- index.js
+++ index.js
@@ -3,11 +3,55 @@ const levels = require("./models/levels.js");
 var mongoUrl;
 
 class DiscordXp {
-  async set setURL(dbUrl) {
+
+  /**
+  * @param {string} [dbUrl] - A valid mongo database URI.
+  */
+
+  async static setURL(dbUrl) {
     if (!dbUrl) throw new TypeError("A database url was not provided.");
     mongoUrl = dbUrl;
     return mognoose.connect(dbUrl, {
       useNewUrlParser: true
     });
+  }
+
+  /**
+  * @param {string} [userId] - Discord user id.
+  * @param {string} [guildId] - Discord guild id.
+  */
+
+  async static createUser(userId, guildId) {
+    if (!userId) throw new TypeError("An user id was not provided.");
+    if (!guildId) throw new TypeError("A guild id was not provided.");
+
+    const isUser = await levels.findOne({ userID: userId, guildID: guildId });
+    if (isUser) return false;
+
+    const newUser = new levels({
+      userID: userId,
+      guildID: guildId
+    });
+
+    await newUser.save().catch(e => console.log(`Failed to create user: ${e}`));
+
+    return newUser;
+  }
+
+  /**
+  * @param {string} [userId] - Discord user id.
+  * @param {string} [guildId] - Discord guild id.
+  */
+
+  async static deleteUser(userId, guildId) {
+    if (!userId) throw new TypeError("An user id was not provided.");
+    if (!guildId) throw new TypeError("A guild id was not provided.");
+
+    const user = await levels.findOne({ userID: userId, guildID: guildId });
+    if (!user) return false;
+
+    await levels.findOneAndDelete({ userID: userId, guildID: guildId }).catch(e => console.log(`Failed to delete user: ${e}`))
+
+    return user;
   }
 }
