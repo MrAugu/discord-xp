9cf6c86ba53cce1f593848f7e33d83fbae4e9f36
MrAugu <mraugu@yahoo.com>
2019-09-27 15:31:13 +0300

    Various Functions

--- index.js
+++ index.js
@@ -50,7 +50,106 @@ class DiscordXp {
     const user = await levels.findOne({ userID: userId, guildID: guildId });
     if (!user) return false;
 
-    await levels.findOneAndDelete({ userID: userId, guildID: guildId }).catch(e => console.log(`Failed to delete user: ${e}`))
+    await levels.findOneAndDelete({ userID: userId, guildID: guildId }).catch(e => console.log(`Failed to delete user: ${e}`));
+
+    return user;
+  }
+
+  /**
+  * @param {string} [userId] - Discord user id.
+  * @param {string} [guildId] - Discord guild id.
+  * @param {number} [xp] - Amount of xp to append.
+  */
+
+  async static appendXp(userId, guildId, xp) {
+    if (!userId) throw new TypeError("An user id was not provided.");
+    if (!guildId) throw new TypeError("A guild id was not provided.");
+    if (!xp) throw new TypeError("An amount of xp was not provided.");
+
+    const user = await levels.findOne({ userID: userId, guildID: guildId });
+    if (!user) return false;
+
+    user.xp += xp;
+
+    user.save().catch(e => console.log(`Failed to append xp: ${e}`) );
+
+    return user;
+  }
+
+  /**
+  * @param {string} [userId] - Discord user id.
+  * @param {string} [guildId] - Discord guild id.
+  * @param {number} [levels] - Amount of levels to append.
+  */
+
+  async static appendLevel(userId, guildId, levels) {
+    if (!userId) throw new TypeError("An user id was not provided.");
+    if (!guildId) throw new TypeError("A guild id was not provided.");
+    if (!levels) throw new TypeError("An amount of levels was not provided.");
+
+    const user = await levels.findOne({ userID: userId, guildID: guildId });
+    if (!user) return false;
+
+    user.level += levels;
+
+    user.save().catch(e => console.log(`Failed to append level: ${e}`) );
+
+    return user;
+  }
+
+  /**
+  * @param {string} [userId] - Discord user id.
+  * @param {string} [guildId] - Discord guild id.
+  * @param {number} [xp] - Amount of xp to set.
+  */
+
+  async static setXp(userId, guildId, xp) {
+    if (!userId) throw new TypeError("An user id was not provided.");
+    if (!guildId) throw new TypeError("A guild id was not provided.");
+    if (!xp) throw new TypeError("An amount of xp was not provided.");
+
+    const user = await levels.findOne({ userID: userId, guildID: guildId });
+    if (!user) return false;
+
+    user.xp = xp;
+
+    user.save().catch(e => console.log(`Failed to set xp: ${e}`) );
+
+    return user;
+  }
+
+  /**
+  * @param {string} [userId] - Discord user id.
+  * @param {string} [guildId] - Discord guild id.
+  * @param {number} [level] - A level to set.
+  */
+
+  async static setLevel(userId, guildId, level) {
+    if (!userId) throw new TypeError("An user id was not provided.");
+    if (!guildId) throw new TypeError("A guild id was not provided.");
+    if (!level) throw new TypeError("A level was not provided.");
+
+    const user = await levels.findOne({ userID: userId, guildID: guildId });
+    if (!user) return false;
+
+    user.level = level;
+
+    user.save().catch(e => console.log(`Failed to set level: ${e}`) );
+
+    return user;
+  }
+
+  /**
+  * @param {string} [userId] - Discord user id.
+  * @param {string} [guildId] - Discord guild id.
+  */
+
+  async static fetch(userId, guildId) {
+    if (!userId) throw new TypeError("An user id was not provided.");
+    if (!guildId) throw new TypeError("A guild id was not provided.");
+
+    const user = await levels.findOne({ userID: userId, guildID: guildId });
+    if (!user) return false;
 
     return user;
   }
