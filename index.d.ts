8bfa31e82bcda3774c45229c424b271bff8fa97a
Nico Finkernagel <nico@gruselhaus.com>
2021-04-03 23:38:03 +0200

    Using the correct number type

--- index.d.ts
+++ index.d.ts
@@ -8,19 +8,19 @@ import { Client } from "discord.js";
 type User = {
   userID: string;
   guildID: string;
-  xp: Number;
-  level: Number;
+  xp: number;
+  level: number;
   lastUpdated: Date;
-  cleanXp: Number;
-  cleanNextLevelXp: Number;
+  cleanXp: number;
+  cleanNextLevelXp: number;
 };
 
 type LeaderboardUser = {
   guildID: string;
   userID: string;
-  xp: Number;
-  level: Number;
-  position: Number;
+  xp: number;
+  level: number;
+  position: number;
   username: String | null;
   discriminator: String | null;
 };
@@ -30,15 +30,15 @@ declare module "discord-xp" {
     static async setURL(dbURL: string): Promise<typeof import("mongoose")>;
     static async createUser(userId: string, guildId: string): Promise<User>;
     static async deleteUser(userId: string, guildId: string): Promise<User>;
-    static async appendXp(userId: string, guildId: string, xp: Number): Promise<boolean>;
-    static async appendLevel(userId: string, guildId: string, levels: Number): Promise<User>;
-    static async setXp(userId: string, guildId: string, xp: Number): Promise<User>;
-    static async setLevel(userId: string, guildId: string, level: Number): Promise<User>;
+    static async appendXp(userId: string, guildId: string, xp: number): Promise<boolean>;
+    static async appendLevel(userId: string, guildId: string, levels: number): Promise<User>;
+    static async setXp(userId: string, guildId: string, xp: number): Promise<User>;
+    static async setLevel(userId: string, guildId: string, level: number): Promise<User>;
     static async fetch(userId: string, guildId: string, fetchPosition = false): Promise<User>;
-    static async subtractXp(userId: string, guildId: string, xp: Number): Promise<User>;
-    static async subtractLevel(userId: string, guildId: string, level: Number): Promise<User>;
-    static async fetchLeaderboard(guildId: String, limit: Number): Promise<User[] | []>;
+    static async subtractXp(userId: string, guildId: string, xp: number): Promise<User>;
+    static async subtractLevel(userId: string, guildId: string, level: number): Promise<User>;
+    static async fetchLeaderboard(guildId: String, limit: number): Promise<User[] | []>;
     static async computeLeaderboard(client: Client, leaderboard: User[], fetchUsers = false): Promise<LeaderboardUser[] | []>;
-    static xpFor(targetLevel: Number): Number;
+    static xpFor(targetLevel: number): number;
   }
 }
