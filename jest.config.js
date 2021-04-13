722519bf4edeb4e40b0fd5760ba6d208bfa2b2c6
MrAugu <39545629+MrAugu@users.noreply.github.com>
2021-04-13 09:14:28 +0300

    Tidy Workspace

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  automock: false,
+  bail: false,
+  moduleFileExtensions: ["js", "json", "jsx", "node"],
+  testMatch: ["**/__tests__/**/*.js?(x)"],
+  testPathIgnorePatterns: ["/node_modules/"],
+  rootDir: "./" 
+};
